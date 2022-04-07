5080344e30c0cb86f20c09c150553b91dbfeb4ac
Yunseo Jeong <samsunghappytree123@naver.com>
2022-04-07 16:00:39 +0900

    feat(config_example): separating classes and adding new variables
    class로 분리 및 webhook_url을 추가하였습니다.

--- config_example.py
+++ config_example.py
@@ -26,9 +26,25 @@ class setting:
 
     dev_ids = []  # 개발자의 디스코드 아이디를 입력해주세요.
 
-    db_host_uri = ""  # 사용할 MongoDB 데이터베이스 호스트를 입력해주세요.
-    db_collect_name = ""  # 사용할 MongoDB 데이터베이스 콜렉션 이름을 입력해주세요.
-
-    koreanbots_token = ""  # 한국 디스코드 리스트 (koreanbots.dev)에서 발급받은 토큰을 입력해주세요. (없으면, 비워주세요.)
+    koreanbots_token = (
+        ""  # 한국 디스코드 리스트 (koreanbots.dev)에서 발급받은 토큰을 입력해주세요. (없으면, 비워주세요.)
+    )
     archiver_token = ""  # Archiver (archiver.me)에서 발급받은 토큰을 입력해주세요. (없으면, 비워주세요.)
     universe_token = ""  # Universlist (universlist.kr)에서 발급받은 토큰을 입력해주세요. (없으면, 비워주세요.)
+
+    webhook_url = ""  # 봇 상태 알림을 위한 웹훅 주소를 입력해주세요. (없으면, 비워주세요.)
+
+    class database:
+        """
+        MongoDB 데이터베이스 설정값입니다.
+        """
+
+        host_uri = ""  # 사용할 MongoDB 데이터베이스 호스트를 입력해주세요.
+        collect_name = ""  # 사용할 MongoDB 데이터베이스 콜렉션 이름을 입력해주세요.
+
+    class log:
+        """
+        로그 관련 설정입니다.
+        """
+
+        name = ""  # 로그 이름을 입력해주세요. (ex. bot)
