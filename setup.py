f05c3fd558bb78bae08b93b3a638057ee1c2a734
samsunghappytree123 <samsunghappytree123@naver.com>
2022-05-06 22:57:32 +0900

    feat(setup): add async

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 import json
 import os
+import asyncio
 
-try:
-    os.popen("mkdir logs")
-    print("logs 폴더 생성 완료")
-except Exception as error:
-    print(error)
 
-try:
-    os.popen("mkdir data")
-    print("data 폴더 생성 완료")
-except Exception as error:
-    print(error)
+async def setup():
+    logs_result = os.popen("mkdir logs")
 
-try:
-    with open("data/voice_channel.json", "w") as f:
-        f.write("{}")
-    print("data/voice_channel.json 생성 완료")
-except Exception as error:
-    print(error)
+    data_result = os.popen("mkdir data")
 
-try:
-    with open("data/command_usages.json", "w") as f:
-        f.write("{}")
-    print("data/command_usages.json 생성 완료")
-except Exception as error:
-    print(error)
+    await asyncio.sleep(1)
+
+    try:
+        with open("data/voice_channel.json", "w") as f:
+            f.write("{}")
+        print("data/voice_channel.json 생성 완료")
+    except Exception as error:
+        print(error)
+
+    try:
+        with open("data/command_usages.json", "w") as f:
+            f.write("{}")
+        print("data/command_usages.json 생성 완료")
+    except Exception as error:
+        print(error)
+
+
+asyncio.run(setup())
