3acecfbf3692724d69605674e3362bd594d10217
samsunghappytree123 <samsunghappytree123@naver.com>
2022-05-06 22:53:16 +0900

    style(cogs.task): black code style

--- cogs/task.py
+++ cogs/task.py
@@ -45,7 +45,9 @@ class task(commands.Cog):
                     "{{서버}}": str(len(self.bot.guilds)),
                     "{{유저}}": str(len(self.bot.users)),
                     "{{샤드}}": str(shard.id + 1),
-                    "{{개발자}}": ', '.join([str(self.bot.get_user(i)) for i in self.bot.owner_ids]),
+                    "{{개발자}}": ", ".join(
+                        [str(self.bot.get_user(i)) for i in self.bot.owner_ids]
+                    ),
                 },
             )
             await self.bot.change_presence(
